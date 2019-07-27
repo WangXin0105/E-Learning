9471a9c8095b85972ce9cd76b9f85eccac3c004d
wangxin <2099743754@qq.com>
2019-07-27 17:38:28 +0800

    【系统管理-页面管理】页面管理的功能实现

--- E-Learning-Service/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/CmsPage.java
+++ E-Learning-Service/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/CmsPage.java
@@ -8,19 +8,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 import java.util.List;
 
-/**
- * @Author: mrt.
- * @Description:
- * @Date:Created in 2018/1/24 10:04.
- * @Modified By:
- */
 @Data
 @ToString
 @Document(collection = "cms_page")
 public class CmsPage {
-    /**
-     * 页面名称、别名、访问地址、类型（静态/动态）、页面模版、状态
-     */
     //站点ID
     private String siteId;
     //页面ID
@@ -56,7 +47,5 @@ public class CmsPage {
     private String htmlFileId;
     //数据Url
     private String dataUrl;
-
-
 
 }
